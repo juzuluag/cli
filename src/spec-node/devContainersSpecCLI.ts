e7af2eed5206aab0fc95852fd60c92906014b2d3
Josh Spicer <josh@joshspicer.com>
2022-05-12 21:15:07 +0000

    implicitly detect features if none passed

--- src/spec-node/devContainersSpecCLI.ts
+++ src/spec-node/devContainersSpecCLI.ts
@@ -74,7 +74,7 @@ export interface FeaturesTestCommandInput {
 	cliHost: CLIHost;
 	baseImage: string;
 	directory: string;
-	features: string;
+	features?: string;
 	remoteUser: string;
 	quiet: boolean;
 }
@@ -92,13 +92,6 @@ async function featuresTest({
 }: FeaturesTestArgs) {
 	const cwd = process.cwd();
 	const cliHost = await getCLIHost(cwd, loadNativeModule);
-
-	console.log(directory)
-
-	if (!features) {
-		process.stderr.write('TODO: Support auto running against all features in ./test if none specified');
-		process.exit(1);
-	}
 
 	const params: FeaturesTestCommandInput = {
 		cliHost,
